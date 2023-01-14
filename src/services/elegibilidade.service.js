1c8bea7b3603af2ab00489b143984b6ebd846a61
Fran Ribeiro <ribeirofrv@gmail.com>
2023-01-14 02:27:58 -0300

    feat: avalia elegibilidade da cliente

---
+++ src/services/elegibilidade.service.js
@@ -0,0 +1,44 @@
+const {
+  checaClasseDeConsumo,
+  checaModalidadeTarifaria,
+  calculaMediaDeConsumo,
+  checaConsumoPorConexao,
+  calculaEnconomiaAnualDeCO2,
+} = require('./helper/criteriosElegibilidade');
+const checaInput = require('./validation/empresa.validation');
+
+const getElegibilidade = async (empresa) => {
+  const {
+    tipoDeConexao,
+    classeDeConsumo,
+    modalidadeTarifaria,
+    historicoDeConsumo,
+  } = empresa;
+
+  await checaInput(empresa);
+
+  const razoesInelegibilidade = [];
+
+  const classeDeConsumoElegivel = checaClasseDeConsumo(classeDeConsumo);
+  if (classeDeConsumoElegivel !== true) razoesInelegibilidade.push(classeDeConsumoElegivel);
+
+  const modalidadeTarifariaElegivel = checaModalidadeTarifaria(modalidadeTarifaria);
+  if (modalidadeTarifariaElegivel !== true) razoesInelegibilidade.push(modalidadeTarifariaElegivel);
+
+  const consumoMinimo = calculaMediaDeConsumo(historicoDeConsumo);
+
+  const consumoPorConexaoElegivel = checaConsumoPorConexao(tipoDeConexao, consumoMinimo);
+  if (consumoPorConexaoElegivel !== true) razoesInelegibilidade.push(consumoPorConexaoElegivel);
+
+  const elegivel = razoesInelegibilidade.length === 0;
+  if (!elegivel) return { elegivel, razoesInelegibilidade };
+
+  const economiaAnualDeCO2 = calculaEnconomiaAnualDeCO2(consumoMinimo);
+
+  return {
+    elegivel,
+    economiaAnualDeCO2,
+  };
+};
+
+module.exports = { getElegibilidade };
